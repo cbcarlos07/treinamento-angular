7f0b1a2038f37a80613e193d3f63f34ba263e3ac
Carlos Bruno <cbcarlos08@gmail.com>
2021-10-06 09:09:54 -0400

    Objto generico

--- angular/typescript/app4.ts
+++ angular/typescript/app4.ts
@@ -1,7 +1,21 @@
-import {ConcessionariaDao} from './ConcessionariaDao'
+//import { PessoaDao } from './PessoaDao';
+//import {ConcessionariaDao} from './ConcessionariaDao'
 import Concessionaria from './Concessionaria'
+import { Dao } from './Dao';
+import Pessoa from './Pessoa';
 
-let dao: ConcessionariaDao = new ConcessionariaDao()
+//let dao: ConcessionariaDao = new ConcessionariaDao()
+//let dao2: PessoaDao = new PessoaDao()
+
 let concessionaria = new Concessionaria('', [])
+let pessoa: Pessoa = new Pessoa('','')
 
-dao.inserir(concessionaria)
+let dao3: Dao<Concessionaria> = new Dao<Concessionaria>()
+let dao4: Dao<Pessoa> = new Dao<Pessoa>()
+
+dao3.inserir(concessionaria)
+dao4.remover(5)
+
+
+
+
--- angular/typescript/dist/app4.js
+++ angular/typescript/dist/app4.js
@@ -3,8 +3,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var ConcessionariaDao_1 = require("./ConcessionariaDao");
+//import { PessoaDao } from './PessoaDao';
+//import {ConcessionariaDao} from './ConcessionariaDao'
 var Concessionaria_1 = __importDefault(require("./Concessionaria"));
-var dao = new ConcessionariaDao_1.ConcessionariaDao();
+var Dao_1 = require("./Dao");
+var Pessoa_1 = __importDefault(require("./Pessoa"));
+//let dao: ConcessionariaDao = new ConcessionariaDao()
+//let dao2: PessoaDao = new PessoaDao()
 var concessionaria = new Concessionaria_1.default('', []);
-dao.inserir(concessionaria);
+var pessoa = new Pessoa_1.default('', '');
+var dao3 = new Dao_1.Dao();
+var dao4 = new Dao_1.Dao();
+dao3.inserir(concessionaria);
+dao4.remover(5);
