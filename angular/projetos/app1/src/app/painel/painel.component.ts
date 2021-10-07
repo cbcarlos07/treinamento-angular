5ea496b48b22bec36852633150de86c0cf595b8d
Carlos Bruno <cbcarlos08@gmail.com>
2021-10-07 14:53:41 -0400

    coracao adicionado

--- angular/projetos/app1/src/app/painel/painel.component.ts
+++ angular/projetos/app1/src/app/painel/painel.component.ts
@@ -10,14 +10,15 @@ import { FRASES } from './frases-mock';
 export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES;
   public instrucao: string = 'Traduza a frase:';
-  public resposta!: string;
+  public resposta: string = ''
 
-  public rodada: number = 0
-  public rodadaFrase: Frase
+  public rodada: number = 0;
+  public rodadaFrase!: Frase;
+
+  public progresso: number = 0
 
   constructor() {
-    this.rodadaFrase = this.frases[this.rodada]
-    console.log(this.rodadaFrase);
+    this.atualizaRodada()
   }
 
   ngOnInit(): void {}
@@ -28,6 +29,32 @@ export class PainelComponent implements OnInit {
   }
 
   public verificarResposta(): void {
-    console.log('Verificar Resposta', this.resposta);
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('A traducao esta correta!');
+      //trocar pergunta da rodada
+      this.rodada++;
+
+      //incrementa a barra de progresso no componente progresso
+      this.progresso = this.progresso + (100 / this.frases.length)
+      console.log(this.progresso);
+      
+      //atualiza o objeto rodadaFrase que contem a frase exibida para o usuario
+      this.atualizaRodada()
+
+      //limpar resposta do usuario
+  
+    } else {
+      alert('A traducao esta errada!');
+    }
   }
+
+  public atualizaRodada(): void {
+
+    //define a frade da rodada com base em algum criterio
+    this.rodadaFrase = this.frases[this.rodada];
+
+     //limpar resposta do usuario
+    this.resposta = ''
+  }
+
 }
