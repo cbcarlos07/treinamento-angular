8e21afd11f5e3a738d9418b040d377226fbe0001
Carlos Bruno <cbcarlos08@gmail.com>
2021-10-11 13:45:39 -0400

    Novos componentes

--- angular/projetos/app2/src/app/home/home.component.ts
+++ angular/projetos/app2/src/app/home/home.component.ts
@@ -18,9 +18,8 @@ export class HomeComponent implements OnInit {
     //this.ofertas = this.ofertasService.getOfertas()
     //console.log(this.ofertas);
 
-     this.ofertasService.getOfertas2()
+     this.ofertasService.getOfertas()
         .then(( ofertas: Oferta[] ) => {
-          console.log('a funcao resolve() foi resolvida depois de 3 segundos')
           this.ofertas = ofertas
         })
         .catch(( param: any ) => { 
--- angular/projetos/app2/src/app/ofertas.service.ts
+++ angular/projetos/app2/src/app/ofertas.service.ts
@@ -7,87 +7,9 @@ export class OfertasService {
 
     constructor(private http: HttpClient) {}
 
-    public ofertas: Array<Oferta> = 
-    [
-        {
-            id: 1,
-            categoria: "restaurante",
-            titulo: "Super Burger",
-            descricao_oferta: "Rodízio de Mini-hambúrger com opção de entrada.",
-            anunciante: "Original Burger",
-            valor: 29.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/1/img1.jpg"},
-                {url: "/assets/ofertas/1/img2.jpg"},
-                {url: "/assets/ofertas/1/img3.jpg"},
-                {url: "/assets/ofertas/1/img4.jpg"}
-            ]
-        },
-        {
-            id: 2,
-            categoria: "restaurante",
-            titulo: "Cozinha Mexicana",
-            descricao_oferta: "Almoço ou Jantar com Rodízio Mexicano delicioso.",
-            anunciante: "Mexicana",
-            valor: 32.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/2/img1.jpg"},
-                {url: "/assets/ofertas/2/img2.jpg"},
-                {url: "/assets/ofertas/2/img3.jpg"},
-                {url: "/assets/ofertas/2/img4.jpg"}
-            ]
-        
-        },
-        {
-            id: 4,
-            categoria: "diversao",
-            titulo: "Estância das águas",
-            descricao_oferta: "Diversão garantida com piscinas, trilhas e muito mais.",
-            anunciante: "Estância das águas",
-            valor: 31.90,
-            destaque: true,
-            imagens: [
-                {url: "/assets/ofertas/3/img1.jpg"},
-                {url: "/assets/ofertas/3/img2.jpg"},
-                {url: "/assets/ofertas/3/img3.jpg"},
-                {url: "/assets/ofertas/3/img4.jpg"},
-                {url: "/assets/ofertas/3/img5.jpg"},
-                {url: "/assets/ofertas/3/img6.jpg"}
-            ]
-        }
-    ]
-    
-    public getOfertas(): Array<Oferta> {
-        return this.ofertas
-    }
-
-    public getOfertas2(): Promise<Oferta[]> {
-        return new Promise((resolve, reject) => {
-            //algum tipo de processamento que ao finalizar chama a funcao resolve ou reject
-            //console.log('serah que passou por aqui?')
-            let deucerto = true
-            if(deucerto) {
-                setTimeout(() => resolve( this.ofertas ) , 3000)
-            } else  {
-                reject({ codigo_erro: 404, mensagem_erro: 'servidor nao encontrado XYZ' })
-            }
-        })
-        .then(( ofertas: any) => {
-            //fazer alguma tratativa
-            console.log('primeiro then')
-            return ofertas
-        })
-        .then((ofertas: any) => {
-            console.log('segundo then')
-            return new Promise((resolve2, reject2) => {
-                setTimeout(() => { resolve2( ofertas ) },  3000)
-            })
-        })
-        .then (( ofertas: any) => {
-            console.log('terceiro then executado apos 3 segundos porque estava aguardando um promisse ser resolvida');
-            return ofertas
-        })
+    public getOfertas(): Promise<Oferta[]> {
+        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+            .toPromise()
+            .then((resposta: any) => resposta)
     }
 }
