86a95654cf6495a28420e0f9ea4fc0172a179a7b
Carlos Bruno <cbcarlos08@gmail.com>
2021-10-11 15:42:25 -0400

    diversao

--- angular/projetos/app2/src/app/diversao/diversao.component.ts
+++ angular/projetos/app2/src/app/diversao/diversao.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from 'src/src/app/ofertas.service';
+import { Oferta } from 'src/src/app/shared/oferta.model';
 
 @Component({
   selector: 'app-diversao',
   templateUrl: './diversao.component.html',
-  styleUrls: ['./diversao.component.css']
+  styleUrls: ['./diversao.component.css'],
+  providers: [OfertasService]
 })
 export class DiversaoComponent implements OnInit {
-
-  constructor() { }
+  ofertas: Oferta[] = []
+  constructor(private _ofertasService: OfertasService) { }
 
   ngOnInit(): void {
+    this._ofertasService.getOfertasPorCategoria('diversao')
+        .then( (_ofertas: Oferta[]) => {
+          console.log(_ofertas);
+          
+            this.ofertas = _ofertas;
+        })
   }
 
 }
