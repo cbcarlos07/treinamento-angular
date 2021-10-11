22e7f4f650de19ba00533a27edd992cd0ed73922
Carlos Bruno <cbcarlos08@gmail.com>
2021-10-11 15:03:44 -0400

    Deafio app2

--- angular/projetos/app2/src/app/restaurantes/restaurantes.component.ts
+++ angular/projetos/app2/src/app/restaurantes/restaurantes.component.ts
@@ -3,20 +3,21 @@ import { Oferta } from '../shared/oferta.model';
 import { OfertasService } from '../ofertas.service';
 
 @Component({
-  selector: 'app-restaurantes',
-  templateUrl: './restaurantes.component.html',
-  styleUrls: ['./restaurantes.component.css'],
-  providers: [ OfertasService ]
+    selector: 'app-restaurantes',
+    templateUrl: './restaurantes.component.html',
+    styleUrls: ['./restaurantes.component.css'],
+    providers: [ OfertasService ]
 })
 export class RestaurantesComponent implements OnInit {
-
-  constructor(private ofertasService: OfertasService) { }
-
-  ngOnInit(): void {
-    this.ofertasService.getOfertasPorCategoria('restaurante')
-      .then(( ofertas: Oferta[]) => {
-          console.log(ofertas);
-      })
-  }
-
+    public ofertas!: Oferta[]
+    constructor(private ofertasService: OfertasService) { }
+    
+    ngOnInit(): void {
+        this.ofertasService.getOfertasPorCategoria('restaurante')
+        .then(( ofertas: Oferta[]) => {
+            console.log(ofertas);
+            this.ofertas = ofertas;
+        })
+    }
+    
 }
