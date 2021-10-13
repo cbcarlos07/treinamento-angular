efb09d19fd200d25af73ef83f83df312018e02d0
Carlos Bruno <cbcarlos08@gmail.com>
2021-10-13 11:22:04 -0400

    Como usar e onde fica adicionados

--- angular/projetos/app2/src/app/app.routes.ts
+++ angular/projetos/app2/src/app/app.routes.ts
@@ -1,3 +1,5 @@
+import { OndeFicaComponent } from './oferta/onde-fica/onde-fica.component';
+import { ComoUsarComponent } from './oferta/como-usar/como-usar.component';
 import { Routes } from "@angular/router";
 
 import { HomeComponent } from "./home/home.component";
@@ -10,5 +12,15 @@ export const ROUTES: Routes = [
     { path: 'restaurantes', component: RestaurantesComponent },
     { path: 'diversao', component: DiversaoComponent},
     { path: 'oferta', component: HomeComponent},
-    { path: 'oferta/:id', component: OfertaComponent}
+    { path: 'oferta/:id', component: OfertaComponent, 
+        children: [
+            {
+                path: '',
+                redirectTo: 'como-usar',
+                pathMatch: 'full'
+            },
+            { path: 'como-usar', component: ComoUsarComponent },
+            { path: 'onde-fica', component: OndeFicaComponent }
+        ]
+    }
 ]
