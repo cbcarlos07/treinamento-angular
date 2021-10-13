b79e4371349a94c84be4263a03e730866dbb72fd
Carlos Bruno <cbcarlos08@gmail.com>
2021-10-13 10:43:58 -0400

    col

--- angular/projetos/app2/src/app/oferta/oferta.component.ts
+++ angular/projetos/app2/src/app/oferta/oferta.component.ts
@@ -7,23 +7,22 @@ import { Oferta } from '../shared/oferta.model';
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
   styleUrls: ['./oferta.component.css'],
-  providers: [OfertasService]
+  providers: [OfertasService],
 })
 export class OfertaComponent implements OnInit {
+  public oferta!: Oferta;
 
-  public oferta!: Oferta
-
-  constructor(private route: ActivatedRoute, 
-              private ofertasService: OfertasService
-              ) { }
+  constructor(
+    private route: ActivatedRoute,
+    private ofertasService: OfertasService
+  ) {}
 
   ngOnInit(): void {
-   console.log('Id recuperado da rota:', this.route.snapshot.params['id'] );
-   this.ofertasService.getOfertaPorId(this.route.snapshot.params['id'])
-        .then((oferta: Oferta) => {
-          this.oferta = oferta;
-          console.log(this.oferta);
-        })
+    console.log('Id recuperado da rota:', this.route.snapshot.params['id']);
+    this.ofertasService
+      .getOfertaPorId(this.route.snapshot.params['id'])
+      .then((oferta: Oferta) => {
+        this.oferta = oferta;
+      });
   }
-
 }
--- angular/projetos/app2/src/app/ofertas.service.ts
+++ angular/projetos/app2/src/app/ofertas.service.ts
@@ -1,26 +1,29 @@
 import { HttpClient } from "@angular/common/http"
 import { Injectable } from "@angular/core"
 import { Oferta } from "./shared/oferta.model"
+import { URL_API } from "./app.api"
 
 @Injectable()
 export class OfertasService {
 
+    //private url_api = 'http://localhost:3000/ofertas'
+
     constructor(private http: HttpClient) {}
 
     public getOfertas(): Promise<Oferta[]> {
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(`${URL_API}?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta)
     }
 
     public getOfertasPorCategoria(categoria: string): Promise<Oferta[]> {
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+        return this.http.get(`${URL_API}?categoria=${categoria}`)
             .toPromise()
             .then((resposta: any) => resposta)
     }
 
     public getOfertaPorId(id: number): Promise<Oferta> {
-        return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+        return this.http.get(`${URL_API}?id=${id}`)
             .toPromise()
             .then((resposta: any) =>  {
                 return resposta[0]
