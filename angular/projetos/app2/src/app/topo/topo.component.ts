3cd34f997075fec9ea20e8ffa4a845c4369e763a
Carlos Bruno <cbcarlos08@gmail.com>
2021-10-13 16:49:36 -0400

    Observable

--- angular/projetos/app2/src/app/topo/topo.component.ts
+++ angular/projetos/app2/src/app/topo/topo.component.ts
@@ -1,16 +1,28 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
   styleUrls: ['./topo.component.css'],
+  providers: [OfertasService]
 })
 export class TopoComponent implements OnInit {
-  constructor() {}
+
+  public ofertas!: Observable<Oferta[]>
+
+  constructor(private ofertasService: OfertasService) {}
 
   ngOnInit(): void {}
 
-  public pesquisa(termoDaBusca: string): void {
-    console.log(termoDaBusca);
+  public pesquisa(termoDaPesquisa: string): void {
+    this.ofertas =  this.ofertasService.pesquisaOfertas(termoDaPesquisa)
+    
+    this.ofertas.subscribe(
+      (ofertas: Oferta[]) => console.log(ofertas)
+    )
+    
   }
 }
