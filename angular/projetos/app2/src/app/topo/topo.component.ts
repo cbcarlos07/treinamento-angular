925705dfe05ab9a29d195fe9f3bb5eb646483eb2
Carlos Bruno <cbcarlos08@gmail.com>
2021-10-14 09:44:56 -0400

    switchMap

--- angular/projetos/app2/src/app/topo/topo.component.ts
+++ angular/projetos/app2/src/app/topo/topo.component.ts
@@ -1,30 +1,53 @@
-import { Observable } from 'rxjs';
+import { Observable, of, Subject } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { OfertasService } from '../ofertas.service';
 import { Oferta } from '../shared/oferta.model';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-topo',
   templateUrl: './topo.component.html',
   styleUrls: ['./topo.component.css'],
-  providers: [OfertasService]
+  providers: [OfertasService],
 })
 export class TopoComponent implements OnInit {
+  public ofertas!: Observable<Oferta[]>;
 
-  public ofertas!: Observable<Oferta[]>
+  private subjectPesquisa: Subject<string> = new Subject<string>()
 
   constructor(private ofertasService: OfertasService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.ofertas = this.subjectPesquisa // retorno do array de Ofertas
+    .pipe(switchMap((termo: string) => {
+      console.log('requisicao http para api  ', termo);
+      if(termo.trim() === '') {
+        //retornar um observable de array de ofertas vazio
+        return of<Oferta[]>([])
+      }
+      return this.ofertasService.pesquisaOfertas(termo)   
+    }))
+
+    this.ofertas.subscribe((ofertas: Oferta[]) => {
+      console.log(ofertas);
+    })
+    
+  }
 
   public pesquisa(termoDaPesquisa: string): void {
-    this.ofertas =  this.ofertasService.pesquisaOfertas(termoDaPesquisa)
-    
-    this.ofertas.subscribe(
-      (ofertas: Oferta[]) => console.log(ofertas),
-      (erro: any) => console.log('Erro status: ', erro.status)
+      console.log('keyup carcter ', termoDaPesquisa);
       
-    )
-    
+      this.subjectPesquisa.next(termoDaPesquisa)
+
+
+
+
+    // this.ofertas = this.ofertasService.pesquisaOfertas(termoDaPesquisa);
+
+    // this.ofertas.subscribe(
+    //   (ofertas: Oferta[]) => console.log(ofertas),
+    //   (erro: any) => console.log('Erro status: ', erro.status),
+    //   () => console.log('fluxo de enventos completo!')
+    // );
   }
 }
