1f11699cf15766e50455b514fd8dab0849171416
Carlos Bruno <cbcarlos08@gmail.com>
2021-10-18 15:39:21 -0400

    validaç~'ao

--- angular/projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
+++ angular/projetos/app2/src/app/ordem-compra/ordem-compra.component.ts
@@ -10,11 +10,21 @@ import { Pedido } from '../shared/pedido.model';
   providers: [OrdemCompraService],
 })
 export class OrdemCompraComponent implements OnInit {
+  public idPedidoCompra!: number;
+
   public formulario: FormGroup = new FormGroup({
-    'endereco': new FormControl(null, [ Validators.required, Validators.minLength(3), Validators.maxLength(120) ]),
-    'numero': new FormControl(null, [ Validators.required, Validators.minLength(1), Validators.maxLength(20) ]),
-    'complemento': new FormControl(null),
-    'formaPagamento': new FormControl(null, [ Validators.required ])
+    endereco: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(120),
+    ]),
+    numero: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(1),
+      Validators.maxLength(20),
+    ]),
+    complemento: new FormControl(null),
+    formaPagamento: new FormControl(null, [Validators.required]),
   });
 
   constructor(private ordemCompraService: OrdemCompraService) {}
@@ -22,14 +32,25 @@ export class OrdemCompraComponent implements OnInit {
   ngOnInit() {}
 
   public confirmarCompra(): void {
-    if(this.formulario.status === 'INVALID') {
-      console.log('formulario esta invalido')
-      this.formulario.get('endereco')?.markAsTouched()
-      this.formulario.get('numero')?.markAsTouched()
-      this.formulario.get('complemento')?.markAsTouched()
-      this.formulario.get('formaPagamento')?.markAsTouched()
+    if (this.formulario.status === 'INVALID') {
+      console.log('formulario esta invalido');
+      this.formulario.get('endereco')?.markAsTouched();
+      this.formulario.get('numero')?.markAsTouched();
+      this.formulario.get('complemento')?.markAsTouched();
+      this.formulario.get('formaPagamento')?.markAsTouched();
     } else {
-      console.log('formulario esta valido!!!!')
+      let pedido: Pedido = new Pedido(
+        this.formulario.value.endereco,
+        this.formulario.value.numero,
+        this.formulario.value.complemento,
+        this.formulario.value.formaPagamento
+      );
+
+      this.ordemCompraService
+        .efetivarCompra(pedido)
+        .subscribe((idPedido: number) => {
+          this.idPedidoCompra = idPedido;
+        });
     }
   }
 }
