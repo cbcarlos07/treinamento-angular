0a05895e5eb9075908bc35c8025f63eb45ae43ad
Carlos Bruno <cbcarlos08@gmail.com>
2021-10-19 08:35:37 -0400

    cart item

--- angular/projetos/app2/src/src/app/app.api.ts
+++
@@ -1,1 +0,0 @@
-export const URL_API = 'http://localhost:3000'
--- angular/projetos/app2/src/src/app/shared/pedido.model.ts
+++
@@ -1,8 +0,0 @@
-export class Pedido {
-    constructor(
-        public endereco: string,
-        public numero: string,
-        public complemento: string,
-        public formaPagamento: string
-    ) { }
-}
